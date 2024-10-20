0f4baf4ab8bdb7ae3ba94992d6cb5cc46720e6b2
Arthur Lage <arthurlage2006@gmail.com>
2024-10-19 21:29:15 -0300

    create repo

---
+++ src/main/java/com/arthurlage/urlshortener/model/Link.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+import java.util.LocalDateTime;
+
+public class Link {
+    private UUID id;
+    private String originalLink;
+    private String shortenedLink;
+    private LocalDateTime expirationDate;
+
+
+}
